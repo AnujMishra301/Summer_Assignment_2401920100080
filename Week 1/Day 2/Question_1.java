37808ff2b8ec17c8d3406b41e418d449981e2bec
Anuj Kumar Mishra <mishraanuj3011@gmail.com>
2026-06-02 23:10:01 +0530

    Added Day2

---
+++ Week 1/Day 2/Question_1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentsum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            currentsum+=nums[i];
+            max=Math.max(currentsum,max);
+            if(currentsum<0){
+                currentsum=0;
+            }
+        }
+        return max;
+    }
+}
