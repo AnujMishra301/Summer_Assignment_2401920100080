23e45ce1325f4e16b0fc3bb24d281d6e1778d448
Anuj Kumar Mishra <mishraanuj3011@gmail.com>
2026-06-05 19:16:31 +0530

    Updated OOPS project

--- Week 1/OOPS/LibraryInterfaceDemo.java
+++ Week 1/OOPS/LibraryInterfaceDemo.java
@@ -4,7 +4,9 @@ public class LibraryInterfaceDemo {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         KidUser kid = new KidUser();
+        AdultUser adult = new AdultUser();
 
+        //test case 1
         System.out.println("Enter your age: ");
         kid.age=sc.nextInt();
 
@@ -13,5 +15,15 @@ public class LibraryInterfaceDemo {
 
         kid.registerAccount();
         kid.requestBook();
+
+        //test case 2
+        System.out.println("Enter your age: ");
+        adult.age=sc.nextInt();
+
+        System.out.println("Enter book type: ");
+        adult.bookType=sc.next();
+
+        adult.registerAccount();
+        adult.requestBook();
     }
 }
