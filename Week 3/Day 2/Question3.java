0da723efb2d50cfb89d757e33dc6f1fadd53a544
Anuj Kumar Mishra <mishraanuj3011@gmail.com>
2026-06-16 14:00:45 +0530

    Added Day 2 of Week 3

---
+++ Week 3/Day 2/Question3.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private ListNode reverse(ListNode head){
+        ListNode curr=head;
+        ListNode prev=null;
+
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        if(head==null||head.next==null) return true;
+        while(fast.next!=null&&fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode mid=reverse(slow.next);
+        ListNode p1=head;
+        ListNode p2=mid;
+        while(p2!=null){
+            if(p1.val!=p2.val) return false;
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return true;
+    }
+}
